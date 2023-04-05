a462bb19171ade7a6e13baeb11b36b3bc3a10776
Artik Bharoliya <artikbharoliya67@gmail.com>
2023-04-05 13:01:40 -0700

    refactor(CharacterClass): add component to display min attributes

--- src/components/CharacterClass.js
+++ src/components/CharacterClass.js
@@ -2,11 +2,16 @@ import { useContext, useEffect, useState } from "react";
 import { Card, Collapse, Grid } from "@mui/material";
 import { PlayerContext } from "../context/PlayerContext";
 
+const PrintableAttributes = ({ minAttributes }) => {
+  const attr = Object.entries(minAttributes)
+    .map(([key, value]) => <p key={key}>{`${key}: ${value}`}</p>)
+  return attr;
+}
+
 const CharacterClass = ({ characterName, minAttributes }) => {
   const [isAchievable, setIsAchievable] = useState(false);
   const [expanded, setExpanded] = useState(false);
   const [attributes] = useContext(PlayerContext);
-
 
 
   useEffect(() => {
@@ -29,7 +34,8 @@ const CharacterClass = ({ characterName, minAttributes }) => {
         </Card >
         <Collapse in={expanded}>
           <Card variant="outlined" sx={{ p: 4, textAlign: 'center' }}>
-            {JSON.stringify(minAttributes)}
+            Minimum Attributes:
+            <PrintableAttributes minAttributes={minAttributes} />
           </Card >
         </Collapse>
       </Grid>
